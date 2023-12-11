671d855af76cf152781ba93b8b77d7037e819aea
IgnacioRG <IgnacioRG@ciencias.unam.mx>
2023-12-11 12:18:06 -0600

    MPF Archive
    Archivos y assets originales del juego Mochilas Por Favor antes de la implementación de su tutorial.

---
+++ ARCHIVE/MochilasPorFavor/Scripts/AdminMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MochilasPorFavor
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AdminMenu : MonoBehaviour
+    {
+        public AudioSource music;
+        public AudioSource engine;
+        public AudioClip engineStartClip;
+        public AudioClip engineLoopClip;
+
+
+        void Start()
+        {
+            GetComponent<AudioSource>().loop = true;
+            StartCoroutine(playEngineSound());
+        }
+
+        IEnumerator playEngineSound()
+        {
+            engine.clip = engineStartClip;
+            engine.Play();
+            yield return new WaitForSeconds(2.7f);
+            engine.clip = engineLoopClip;
+            engine.Play();
+            music.Play();
+        }
+
+
+        public void ChangeScene(string sceneName)
+        {
+            Debug.Log("Changing Scene to " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+
+
+    }
+}
---
+++ ARCHIVE/MochilasPorFavor/Scripts/LevelInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public readonly struct LevelInfo
+{
+
+    public LevelInfo(int level,  int numObjsMochila, int numObjsDebeLlevar, int numObjsNoLlevar, int maxFaltan, int maxNoLlevar, float probAceptar, float probFaltan, float probNoLlevar)
+    {
+        Level = level;
+        NumObjsMochila = numObjsMochila;
+        NumObjDebeLlevar = numObjsDebeLlevar;
+        NumObjsNoLlevar = numObjsNoLlevar;
+        MaxFaltan = maxFaltan;
+        MaxNoLlevar = maxNoLlevar;
+        ProbAceptar = probAceptar;
+        ProbFaltan = probFaltan;
+        ProbNoLlevar = probNoLlevar;
+    }
+
+    public int Level { get; }
+    public int NumObjsMochila { get; }
+    public int NumObjDebeLlevar { get; }
+    public int NumObjsNoLlevar { get; }
+    public int MaxFaltan { get; }
+    public int MaxNoLlevar { get; }
+    public float ProbAceptar { get; }
+    public float ProbFaltan { get; }
+    public float ProbNoLlevar { get; }
+
+
+}
